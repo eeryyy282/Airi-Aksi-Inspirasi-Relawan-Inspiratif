3118f7c2ef55d12a84348dc210f14cfb677f7366
Juzairi Safitli <airiagustus82@gmail.com>
2023-12-26 12:47:39 +0700

    menambahklan halaman index utama

---
+++ index.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home Page</title>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Nunito">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+<div id="home">
+    <div id="navigasi">
+        <div id="logo"><img src="src/logoAIRItransparan.png" alt="logo airi"></div>
+        <div id="berandaAktif"><a href="index.php">BERANDA</a></div>
+        <div id="tentangAiri"><a href="tentangAiri.php">TENTANG AIRI</a></div>
+        <div id="sikapDasar"><a href="sikapDasar.php">SIKAP DASAR</a></div>
+        <div id="panduan"><a href="panduan.php">PANDUAN</a></div>
+        <div id="faq"><a href="faq.php">FAQ</a></div>
+        <div id="ulasan"><a href="ulasan.php">ULASAN</a></div>
+        <div id="kontak"><a href="kontak.php">KONTAK</a></div>
+        <div id="daftar"><a href="daftar.php">DAFTAR</a></div>
+    </div>
+    <div id="baner">
+        <div id="backgroundBaner">
+            <div id="judul1Baner">BANTU MEREKA<div id="backgroundMerah">MERAIH MIMPI!</div></div>
+            <div id="deskripsiBaner">Jadilah relawan yang menginspirasi pahlawan masa depan bersama AIRI (Aksi Inspirasi Relawan Inspiratif)!</div>
+            <div id="bergabung"><a href="daftar.php">BERGABUNG</a></div>
+        </div>
+    </div>
+    <div id="manfaat">
+        <div id="manfaat1">
+            <div id="manfaat1gambar"><img src="src/positive-vote.png" alt="manfaat1"></div>
+            <div id="manfaat1deskirpsi">Berbagi Pengalaman Positif</div>
+        </div>
+        <div id="manfaat1">
+            <div id="manfaat1gambar"><img src="src/scholarship.png" alt="manfaat2"></div>
+            <div id="manfaat1deskirpsi">Meningkatkan Kualitas Pendidikan</div>
+        </div>
+        <div id="manfaat1">
+            <div id="manfaat1gambar"><img src="src/public-service.png" alt="manfaat3"></div>
+            <div id="manfaat1deskirpsi">Menumbuhkan Kepedulian Sosial</div>
+        </div>
+        <div id="manfaat1">
+            <div id="manfaat1gambar"><img src="src/love.png" alt="manfaat4"></div>
+            <div id="manfaat1deskirpsi">Waktu Luang yang Berarti</div>
+        </div>
+        <div id="manfaat1">
+            <div id="manfaat1gambar"><img src="src/knowledge.png" alt="manfaat5"></div>
+            <div id="manfaat1deskirpsi">Peluang untuk Saling Belajar</div>
+        </div>
+    </div>
+    <div id="DeskripsiTengah">
+        <div id="judulDeskripsiTengah">
+            <div id="judulDeskripsiTengah1">Ceritamu,</div><div id="judulDeskripsiTengah2">kekuatanmu!</div>
+        </div>
+        <div id="judul2DeskripsiTengah">Bersama AIRI “Aksi Inspirasi Relawan Inspiratif”</div>
+        <div id="deskripsiTengah">Berbagi cerita dengan masa depan bangsa.
+Jadilah bagian dari relawan yang akan menanam kekuatan masa depan!
+Sudah saatnya Anda turut mengambil peran dalam pendidikan masa depan bangsa.</div>
+    </div>
+    <div id="backgroundDeskripsiTengah">
+    </div>
+    <div id="videoYoutube">
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/jia3fhBQ8qI" frameborder="0" allowfullscreen></iframe>
+    </div>
+    <div id="deskripsiBawah">
+        <div id="teksDeskripsiBawah">Kelas AIRI menyajikan solusi inovatif bagi para profesional Indonesia yang ingin memberikan kontribusi positif melalui kegiatan pengajaran di komunitas mereka. Program ini membuka peluang interaksi yang bermanfaat antara para profesional dan sekolah yang menjadi tempat mereka berbagi pengetahuan. Keterlibatan mereka dalam mengambil cuti sehari untuk berbagi pengalaman dengan siswa SD mencerminkan partisipasi individu yang mendalam, menunjukkan tingginya tingkat kesadaran dan kepedulian pribadi terhadap dunia pendidikan.</div>
+    </div>
+    <div id="footer">
+        <div id="facebook"><img src="src/facebook.svg" alt="facebook">@Kelas_AIRI</div>
+        <div id="instagram"><img src="src/instagram.svg" alt="instagram">@KelasAIRI_Official</div>
+        <div id="youtube"><img src="src/youtube.svg" alt="youtube">@BersamaAIRI</div>
+        <div id="copyright">©️ Copyright 2023 Kelas AIRI </div>
+    </div>
+</div>
+</body>
+</html>
